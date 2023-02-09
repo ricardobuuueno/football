e93fb5fb2af81ae0a87bc087a9fc6596aae12ace
Ricardo Bueno <ricardo.bueno.sf.dev@hotmail.com>
2023-02-08 23:38:47 -0300

    plog library added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 
 #include <iostream>
 #include "classes/club.hpp"
+#include "plog/Log.h"
+#include "plog/Initializers/RollingFileInitializer.h"
 
 int main()
 {
 	using namespace football;
+
+	plog::init(plog::debug, "log.csv", 5000, 3);
 
 	club sp{"São Paulo"};
 
